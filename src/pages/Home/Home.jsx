578cda3b3d251d80d91d1eb1088bed0634ea2946
adityagawraw <adityagawraw006@gmail.com>
2023-05-05 10:35:29 +0530

    bug fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import classes from "./Home.module.css"
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 const Home = () => {
     const state = useSelector(state=>state.auth);
     const navigate = useNavigate()
-   if(!state.isAuth) 
-   {
-    navigate('/login');
-   }
+    console.log(state.isAuth);
+    useEffect(()=>{
+        if(!state.isAuth) 
+        {
+         console.log('navigate to login page')
+         navigate('/login');
+        }
+    },[]);
+  
   return (
     <div className={classes.home}>
         Hello World 
